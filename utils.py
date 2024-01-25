4979d674805afc31855329dc337e1832f3fcf19b
piygr <grover1piyush@gmail.com>
2024-01-25 23:07:07 +0530

    updated code

--- utils.py
+++ utils.py
@@ -126,11 +126,11 @@ def generate_output(model, tokenizer, length, input_ids=None, image_features=Non
             #assert predicted_token_logits.size(1) == labels.size(1)
             labels = labels.type(torch.LongTensor).to(device)
 
-            logits = logits[..., ie_size:ie_size+label_size, :].contiguous()
-            labels = labels.contiguous()
+            logits = logits[..., ie_size:ie_size+label_size, :] #.contiguous()
+            #labels = labels.contiguous()
 
-            loss = model.loss(logits.view(-1, logits.size(-1)),
-                              labels.view(-1) )
+            loss = model.loss(logits,
+                              labels )
 
             out = dict(pred=predicted_tokens,
                        loss=loss,
